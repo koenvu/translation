40b4085567e40687179db01794a7875c3a8ea8c6
Koen van Urk <git@ehlo-localhost.com>
2017-04-17 13:16:36 +0200

    update for laravel 5.4

--- src/Loaders/CacheLoader.php
+++ src/Loaders/CacheLoader.php
@@ -80,4 +80,14 @@ class CacheLoader extends Loader implements LoaderInterface
         $this->hints[$namespace] = $hint;
         $this->fallback->addNamespace($namespace, $hint);
     }
+
+    /**
+     * Get an array of all the registered namespaces.
+     *
+     * @return array
+     */
+    public function namespaces()
+    {
+        return $this->hints;
+    }
 }
